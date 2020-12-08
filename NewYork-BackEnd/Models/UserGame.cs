1fa2b059ed83d2e480234ad7ed19690ffa3faa14
Iebe <iebe.1@hotmail.com>
2020-12-08 08:46:17 +0100

    Tussentabel tussen user en game

---
+++ NewYork-BackEnd/Models/UserGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewYork_BackEnd.Models
+{
+    public class UserGame
+    {
+        public int UserGameID { get; set; }
+        public int UserID { get; set; }
+        public int GameID { get; set; }
+    }
+}
