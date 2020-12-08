86953ab3e837c1c9ba3c0661e8b40889a1df6529
kevin <kevin@KEVIN-MBP>
2020-12-08 08:59:43 +0100

    Seed database update

--- NewYork-BackEnd/Models/DBInitializer.cs
+++ NewYork-BackEnd/Models/DBInitializer.cs
@@ -19,51 +19,135 @@ namespace NewYork_BackEnd.Models
             {
                 return;   // DB has been seeded
             }
-
-            string password = "admin";
-
-            byte[] salt = new byte[128 / 8];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(salt);
-            }
-
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
-                iterationCount: 10000,
-                numBytesRequested: 256 / 8));
-
+            #region Users
+            byte[] saltAdmin = Hashing.getSalt();
             context.Users.AddRange(
                 new User
                 {
                     FirstName = "admin",
                     LastName = "admin",
                     Email = "admin@admin.be",
-                    Password = hashed,
-                    HashSalt = salt,
+                    Password = Hashing.getHash("admin",saltAdmin),
+                    HashSalt = saltAdmin,
                     DateOfBirth = DateTime.Now,
                     Role = "admin",
                     TeamID = null
                 });
+            byte[] saltUser1 = Hashing.getSalt();
+            context.Users.AddRange(
+                new User
+                {
+                    FirstName = "user1",
+                    LastName = "user1",
+                    Email = "user1@testing.be",
+                    Password = Hashing.getHash("user1", saltUser1),
+                    HashSalt = saltUser1,
+                    DateOfBirth = DateTime.Now,
+                    Role = "user",
+                    TeamID = null
+                });
+            byte[] saltUser2 = Hashing.getSalt();
+            context.Users.AddRange(
+                new User
+                {
+                    FirstName = "user2",
+                    LastName = "user2",
+                    Email = "user2@testing.be",
+                    Password = Hashing.getHash("user2", saltUser2),
+                    HashSalt = saltUser2,
+                    DateOfBirth = DateTime.Now,
+                    Role = "user",
+                    TeamID = null
+                });
+            byte[] saltUser3 = Hashing.getSalt();
+            context.Users.AddRange(
+                new User
+                {
+                    FirstName = "user3",
+                    LastName = "user3",
+                    Email = "user3@testing.be",
+                    Password = Hashing.getHash("user3", saltUser3),
+                    HashSalt = saltUser3,
+                    DateOfBirth = DateTime.Now,
+                    Role = "user",
+                    TeamID = null
+                });
+            #endregion
+            #region Team
+            context.AddRange(
+                new Team
+                {
+                    TeamName = "Team1",
+                    CompanyName = "Company1",
+                    Address = "Address Team 1",
+                    Photo = "Team1.jpg",
+                    CaptainID = 2
+                });
+            context.AddRange(
+                new Team
+                {
+                    TeamName = "Team2",
+                    CompanyName = "Company2",
+                    Address = "Address Team 2",
+                    Photo = "Team2.jpg",
+                    CaptainID = 3
+                });
 
-            /*context.Tags.AddRange(
-                new Tag { Name = "Sport" },
-                new Tag { Name = "Film" },
-                new Tag { Name = "Reizen" },
-                new Tag { Name = "Games" }
-                );
+            #endregion
+            #region Table
+            context.AddRange(
+                new Table
+                {
+                    TableName = "Table 1",
+                    CompanyName = "Company1",
+                    Address = "Address Company 1",
+                    Photo = "Table1.jpg",
+                    ManagerID = 1
+                });
+            context.AddRange(
+                new Table
+                {
+                    TableName = "Table 2",
+                    CompanyName = "Company2",
+                    Address = "Address Company 2",
+                    Photo = "Table2.jpg",
+                    ManagerID = 2
+                });
+            #endregion
+            #region competition
+            context.AddRange(
+                new Competition
+                {
+                    Address = "Addres competition 1",
+                    Name = "Competition 1"
+                });
+            context.AddRange(
+                new Competition
+                {
+                    Address = "Addres competition 2",
+                    Name = "Competition 2"
+                });
 
-            context.ArticleStatuses.AddRange(
-                new ArticleStatus { Name = "Draft" },
-                new ArticleStatus { Name = "To review" },
-                new ArticleStatus { Name = "Published" }
-                );
+            #endregion
+            #region ranking
+            context.AddRange(
+                new Ranking
+                {
+                    Points = 0,
+                    TeamID = 1,
+                    CompetitionID = 1
+                });
+            context.AddRange(
+                new Ranking
+                {
+                    Points = 0,
+                    TeamID = 2,
+                    CompetitionID = 1
+                });
+            #endregion
+            #region Game
 
-            context.Articles.AddRange(
-                new Article { UserID = 1, Title = "Messi verlaat FC Barçelona", SubTitle = "Messi stuurde een fax met de boodschap dat hij wilt vertrekken.", ArticleStatusID = 1, TagID = 1, Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat non justo dignissim varius. Morbi finibus magna non neque bibendum efficitur. Aliquam eu auctor sem, ut mollis erat. Donec ornare dolor ex, tincidunt blandit purus sodales id. Phasellus a hendrerit libero. Nunc eu ultrices libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer consequat egestas dui sit amet dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In sit amet cursus elit, eu dignissim elit. Ut aliquam cursus urna ultricies rhoncus. Proin vitae neque erat. Sed mollis consectetur diam eget vestibulum." }
-                );*/
+            #endregion
 
             context.SaveChanges();
         }
